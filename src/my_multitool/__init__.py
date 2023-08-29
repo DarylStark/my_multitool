efb83e6e12f0e53cbe86eddea40790d6fa951308
Daryl Stark <daryl.stark@hightechcampus.com>
2023-08-29 09:49:40 +0000

    Version 0.0.1-dev

--- src/my_multitool/__init__.py
+++ src/my_multitool/__init__.py
@@ -4,4 +4,4 @@ Multitool for the My Project. Can be used to do a few tasks, like creating the
 database schema and updating the credentials for users.
 """
 
-__version__ = '0.0.0-dev'
+__version__ = '0.0.1-dev'
