a5461218df412df5f9236000e311ff6d43978607
Daryl Stark <github@dstark.nl>
2023-12-18 20:04:17 +0000

    Fixed linting errors

--- src/my_multitool/tool.py
+++ src/my_multitool/tool.py
@@ -2,20 +2,16 @@
 
 Exposes the `tool` commands for the CLI app.
 """
-from logging import getLogger
-
 import typer
-
-from .style import get_table, ConsoleFactory
-
-from . import __version__ as my_multitool_version
-from my_data import __version__ as my_data_version
-from my_model import __version__ as my_model_version
+from my_data import __version__ as my_data_version # type:ignore
+from my_model import __version__ as my_model_version # type:ignore
 from pydantic import __version__ as pydantic_version
-from sqlmodel import __version__ as sqlmodel_version
 from sqlalchemy import __version__ as sqlalchemy_version
+from sqlmodel import __version__ as sqlmodel_version
 from typer import __version__ as typer_version
 
+from . import __version__ as my_multitool_version
+from .style import ConsoleFactory, get_table
 
 app = typer.Typer(no_args_is_help=True)
 
@@ -26,13 +22,12 @@ def version() -> None:
 
     Shows version information for the tool and all related libraries.
     """
-    logger = getLogger('tool-version')
     console = ConsoleFactory.get_console()
 
     table = get_table()
     table.add_column('Library')
     table.add_column('Version')
-    
+
     table.add_row('My Model', my_model_version)
     table.add_row('My Data', my_data_version)
     table.add_row('My Multitool', my_multitool_version)
