b65cfb4ae40d4a98d5cff1660196796fa074a890
Daryl Stark <github@dstark.nl>
2023-12-20 20:54:01 +0100

    Issue 28/unit testing (#29)
    * Refactored code a bit for more clearity
    
    * Created unit test for the version
    
    * Renamed testfile
    
    * Organized imports
    
    * Refactored regex
    
    * Different Python version for tests
    
    * Different Python version for tests
    
    * Added some comments
    
    Fixes #28

---
+++ tests/test_cli_version.py
@@ -0,0 +1,27 @@
+"""Module to test the `version` command of the CLI script.
+
+Test the `version` command of the script.
+"""
+
+import re
+
+from typer.testing import CliRunner
+
+from my_multitool import __version__ as mymt_version
+from my_multitool.__main__ import app
+
+runner = CliRunner(echo_stdin=True)
+
+
+def test_version() -> None:
+    """Run the `version` subcommand of the script.
+
+    Tests if we get the correct version back and if the returncode for the
+    command is 0 (= no error).
+    """
+    result = runner.invoke(app, ['version'])
+    assert result.exit_code == 0
+    assert len(re.findall(
+        r'^\s*My Multitool\s+' + mymt_version + r'\s+$',
+        result.output,
+        re.MULTILINE)) == 1
