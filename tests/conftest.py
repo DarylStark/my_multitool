9401977f841565dd0d494cbb6dd6e6f2d52963e7
Daryl Stark <daryl@dstark.nl>
2023-12-28 20:28:32 +0000

    Added unit tests for the `database` subcommand

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,46 @@
-import pytest
-from my_multitool.config import ConfigManager, ContextModel
-from my_multitool.globals import config
 import pathlib
+
+import pytest
+from my_data.my_data import MyData
+
+from my_multitool.config import ConfigManager, ContextModel
+from my_multitool.globals import config, get_global_data_object
 
 
 @pytest.fixture
 def config_object(tmp_path: pathlib.Path) -> ConfigManager:
+    """Fixture for a global config object.
+
+    Args:
+        tmp_path: a temporary path created by PyTest.
+
+    Returns:
+        The created config object.
+    """
     config.configure(f'{tmp_path}/.my_multitool_config.yaml')
     config.set_default_config()
     config.full_config.contexts.extend([
         ContextModel(name='context_01', db_string='sqlite:///:memory:/'),
-        ContextModel(name='context_02', db_string='sqlite:///:memory:/'),
+        ContextModel(name='context_02',
+                     db_string='sqlite:///:memory:/', warning=True),
         ContextModel(name='context_03', db_string='sqlite:///:memory:/'),
         ContextModel(name='context_04', db_string='sqlite:///:memory:/'),
         ContextModel(name='context_05', db_string='sqlite:///:memory:/')]
     )
     config.save()
     return config
+
+
+@pytest.fixture
+def data_object(config_object: ConfigManager) -> MyData:
+    """Fixture for a global data object.
+
+    Args:
+        config_object: the fixture for the config object. We don't really use
+            this, we just need it to make sure the correct configuration is
+            created.
+
+    Returns:
+        The created MyData object.
+    """
+    return get_global_data_object()
---
+++ tests/test_cli_database.py
@@ -0,0 +1,94 @@
+"""Tests to test the `database` subcommand for the tool."""
+import pytest
+from my_data.my_data import MyData
+from rich.console import Console
+from typer.testing import CliRunner
+
+from my_multitool.__main__ import app
+from my_multitool.globals import config
+
+from typing import Generator
+
+runner = CliRunner(echo_stdin=True)
+
+
+def test_database_creation(data_object: MyData) -> None:
+    """Test the creation of the database.
+
+    Args:
+        data_object: fixture for the data object.
+    """
+    result = runner.invoke(app, ['database', 'create'])
+    assert result.exit_code == 0
+    assert result.stdout.strip() == 'Created tables'
+
+
+@pytest.mark.parametrize('answer', [
+    'Y', 'y', '', ' Y ', ' y ', '     '
+])
+def test_database_creation_with_warning_confirm(
+    data_object: MyData,
+    answer: str
+) -> None:
+    """Test the creation of the database with a warning and a confirmation.
+
+    Args:
+        data_object: fixture for the data object.
+        answer: the answer to give to the continue question.
+    """
+    def replacement_input(*args, **kwargs):
+        return answer
+
+    Console.input = replacement_input
+
+    config.active_context.warning = True
+    result = runner.invoke(app, ['database', 'create'])
+    assert result.exit_code == 0
+    assert result.stdout.strip() == 'Created tables'
+
+
+@pytest.mark.parametrize('answer', [
+    'N', 'n', 'x'
+])
+def test_database_creation_with_warning_not_confirm(
+    data_object: MyData,
+    answer: str
+) -> None:
+    """Test the creation of the database with a warning and not a confirmation.
+
+    Args:
+        data_object: fixture for the data object.
+        answer: the answer to give to the continue question.
+    """
+    def replacement_input(*args, **kwargs):
+        return answer
+
+    Console.input = replacement_input
+
+    config.active_context.warning = True
+    result = runner.invoke(app, ['database', 'create'])
+    assert result.exit_code == 0
+    assert result.stdout.strip() == ''
+
+
+def test_database_creation_with_drop_tables(data_object: MyData) -> None:
+    """Test the creation of the database while dropping tables.
+
+    Args:
+        data_object: fixture for the data object.
+    """
+    result = runner.invoke(app, ['database', 'create', '--drop-tables'])
+    assert result.exit_code == 0
+    assert result.stdout.strip() == 'Created tables'
+
+
+def test_database_creation_with_create_data(data_object: MyData) -> None:
+    """Test the creation of the database while creating data.
+
+    Args:
+        data_object: fixture for the data object.
+    """
+    result = runner.invoke(app, ['database', 'create', '--create-data'])
+    assert result.exit_code == 0
+    assert 'Created tables' in result.stdout.strip()
+    assert 'Created testdata' in result.stdout.strip()
