27aa1056e867775c6e7725355766867ca922c5ca
Daryl Stark <daryl@dstark.nl>
2024-01-05 23:17:36 +0100

    Version 1.0.2

--- src/my_multitool/__init__.py
+++ src/my_multitool/__init__.py
@@ -4,4 +4,4 @@ Multitool for the My Project. Can be used to do a few tasks, like creating the
 database schema and updating the credentials for users.
 """
 
-__version__ = '1.0.2-dev'
+__version__ = '1.0.2'
