2271d525b19c0b386f5b85e79b49b0d8f10a24fa
Daryl Stark <github@dstark.nl>
2024-02-13 10:20:54 +0000

    Renamed modules

--- src/my_multitool/__main__.py
+++ src/my_multitool/__main__.py
@@ -18,13 +18,13 @@ from typer import __version__ as typer_version
 
 from . import __version__ as my_multitool_version
 from .cli_config import app as config_app
-from .database import app as database_app
+from .cli_database import app as database_app
 from .exceptions import (ConfigFileNotFoundException,
                          ConfigFileNotValidException, GenericCLIException,
                          NoConfirmationException)
 from .globals import config
 from .style import ConsoleFactory, get_table, print_error
-from .users import app as users_app
+from .cli_users import app as users_app
 
 # Create the Typer App
 app = typer.Typer(no_args_is_help=True)
